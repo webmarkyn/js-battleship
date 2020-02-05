09bf17988d84e6ec9df69fa2ca446681e544b188
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-02-04 20:49:27 -0800

    enemy method refactor

--- src/js/dom.js
+++ src/js/dom.js
@@ -67,19 +67,20 @@ const renderSelfBoard = () => {
 };
 
 const renderEnemyBoard = () => {
-    const selfBoard = document.createElement('div');
+    const enemyBoard = document.createElement('div');
     const title = document.createElement('h2');
 
-    selfBoard.id = 'selfBoard';
-    selfBoard.classList.add('self-board');
+    enemyBoard.id = 'enemyBoard';
+    enemyBoard.classList.add('enemy-board');
     title.innerText = 'My Board';
 
-    selfBoard.appendChild(title);
-    selfBoard.appendChild(horizontalHead());
-    selfBoard.appendChild(verticalHead());
-    selfBoard.appendChild(grid());
-    containerAppend(selfBoard);
+    enemyBoard.appendChild(title);
+    enemyBoard.appendChild(horizontalHead());
+    enemyBoard.appendChild(verticalHead());
+    enemyBoard.appendChild(grid());
+    containerAppend(enemyBoard);
 };
+
 export {
     renderSelfBoard,
     renderEnemyBoard
