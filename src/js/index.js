725773d07aad4e5e0ba2056c5910aa99651113ff
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-02-04 22:12:29 -0800

    commets

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,8 @@ import { renderControls, renderEnemyBoard, renderSelfBoard } from './dom';
 renderSelfBoard();
 renderEnemyBoard();
 renderControls();
+
+// TODO: computer player
+// TODO: idea: create predefined board templates for computer player
+// TODO: add player ships (3 ships for a 5 x 5 board)
+// TODO: winning method
