c33cba9ae788d6297c9568c8c3c418876ba39d3f
webmarkyn <web.markyn@gmail.com>
2020-02-10 17:44:58 +0300

    one coma to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
