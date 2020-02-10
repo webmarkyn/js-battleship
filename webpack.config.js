767ae13af42edeee43c83fff0241f55101800e74
webmarkyn <web.markyn@gmail.com>
2020-02-10 17:41:41 +0300

    fixed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    'file-loader'
-                ],
-            }
-        ],
-    },
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader']
+      }
+    ]
+  }
 };
